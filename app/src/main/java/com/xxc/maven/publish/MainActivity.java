350d1a16b4ddd0db935af8edd6e566638fb2c004
徐星晨 <xuxingchen@hinterstellar.com>
2019-09-24 18:35:23 +0800

    添加依赖

--- app/src/main/java/com/xxc/maven/publish/MainActivity.java
+++ app/src/main/java/com/xxc/maven/publish/MainActivity.java
@@ -2,6 +2,7 @@ package com.xxc.maven.publish;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Gson gson = new Gson();
     }
 }
